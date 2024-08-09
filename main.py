ce2a7790f28c508d43bafd122c132b8f6cd42b8a
accelia <accelia@accelia.net>
2024-08-09 16:25:54 +0900

    add args

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    # parser.add_argument("--model", type=str, required=True, help="Path to ONNX model")
+    parser.add_argument("--model", type=str, required=True, help="Path to ONNX model")
     parser.add_argument("--conf", type=float, default=0.25, help="Confidence threshold")
     parser.add_argument("--iou", type=float, default=0.45, help="NMS IoU threshold")
     args = parser.parse_args()
@@ -15,7 +15,8 @@ def main():
     cap = cv2.VideoCapture(0)
 
     # Initialize YOLOv5 Instance Segmentator
-    model_path = "./weights/yolov8s-seg.onnx"
+    # model_path = "./weights/yolov8s-seg.onnx"
+    model_path = args.model
     yoloseg = YOLOSeg(model_path, conf_threshold=args.conf, iou_threshold=args.iou)
 
     cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
@@ -28,7 +29,8 @@ def main():
             break
         
         # Update object localizer
-        boxes, scores, class_ids, masks = yoloseg(frame)        
+        boxes, scores, class_ids, masks = yoloseg(frame)     
+        print("class_ids", class_ids)
         # combined_img = yoloseg.draw_masks(frame)
         combined_img = yoloseg.draw_masks_only(frame)
         cv2.imshow("Detected Objects", combined_img)
